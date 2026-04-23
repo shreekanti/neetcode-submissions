4e5498132a8dc297ff0fdcf66555f33301374809
shreekanti <shreekanti97@gmail.com>
2026-04-23 23:47:49 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+        dp[n][0] = dp[n][1] =0;
+        for(int ind=n-1;ind>=0;ind--) {
+            for(int buy=0;buy<=1;buy++) {
+               int profit=0;
+               if(buy==1) {
+                 profit = Math.max(-prices[ind] + dp[ind+1][0], 0+dp[ind+1][1]);
+               }else {
+                 profit = Math.max(prices[ind]+dp[ind+1][1], 0+dp[ind+1][0]);
+                }
+               dp[ind][buy] =  profit;
+            }
+            
+        }
+        return dp[0][1];
+    }
+}
