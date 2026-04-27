6535fc46c6487732052bbfd2b2a750004e7db1a0
shreekanti <shreekanti97@gmail.com>
2026-04-28 00:45:00 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int n1 =s.length();
+        int n2 = t.length();
+        if(n2>n1) return "";
+        int sInd =-1;
+        int minLen = Integer.MAX_VALUE;
+        int l=0;
+        int[] hash = new int[256];
+        int count=0;
+        for(int i=0;i<n2;i++) {
+            hash[t.charAt(i)]++;
+        }
+
+        for(int r=0;r<n1;r++) {
+            if(hash[s.charAt(r)]>0) {
+                count++;
+            }
+            hash[s.charAt(r)]--;
+            while(count==n2) {
+                if(r-l+1<minLen) {
+                    minLen=r-l+1;
+                    sInd=l;
+                }
+            
+                hash[s.charAt(l)]++;
+                if(hash[s.charAt(l)]>0) {
+                    count--;
+                }
+                l++;
+            }
+        }
+        return sInd==-1?"":s.substring(sInd,sInd+minLen);
+    }
+}
