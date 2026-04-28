adc19870802a8736f8b5f024fe188fce2e96b2d5
shreekanti <shreekanti97@gmail.com>
2026-04-29 00:21:39 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0;i<n;i++) {
+            char c = s.charAt(i);
+            if(c=='(' || c=='{' || c=='[') {
+                st.push(c);
+            }else if(!st.isEmpty() && ((c == ')' && st.peek()=='(') || (c=='}' && st.peek()=='{') ||
+            (c==']' && st.peek()=='['))) {
+                st.pop();
+            }else {
+                return false;
+            }
+        }
+        return st.isEmpty();
+        
+    }
+}
