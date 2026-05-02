5b3b99f960a868900231868e1c280b77a64638c8
shreekanti <shreekanti97@gmail.com>
2026-05-02 11:05:10 +0530

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> numS = new Stack<>();
+        Stack<String> ss = new Stack<>();
+
+        int n = s.length();
+        int num =0;
+        String curr ="";
+
+        for(int i=0;i<n;i++) {
+            char c = s.charAt(i);
+            if(Character.isDigit(c)) {
+                num = num*10 + (c-'0');
+            }else if(c=='[') {
+                numS.push(num);
+                ss.push(curr);
+                num =0;
+                curr="";
+            }else if(c==']') {
+                int count = numS.pop();
+                String prev = ss.pop();
+
+                StringBuilder sb = new StringBuilder(prev);
+
+                for(int j=0;j<count;j++) {
+                    sb.append(curr);
+                }
+                curr = sb.toString();
+            }else {
+                curr+=c;
+            }
+        }
+        return curr;
+    }
+    //tc o(n) sc -O(n)
+}
