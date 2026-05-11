b8637822ae5bd10e290d91203e64f9dd42e7109c
shreekanti <shreekanti97@gmail.com>
2026-05-12 04:17:12 +0530

    Add: split-array-largest-sum - submission-1

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-1.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int n = nums.length;
+        int max =0;
+        int sum =0;
+        if(n<k) return -1;
+        for(int i=0;i<n;i++) {
+            max =Math.max(max, nums[i]);
+            sum +=nums[i];
+        }
+
+
+        int l = max;
+        int h = sum;
+
+        while(l<=h) {
+            int mid = (l+h)/2;
+
+            int countK = fun(nums, mid);
+
+            if(countK<=k) {
+                h=mid-1;
+            }else {
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+
+    public int fun(int[] nums, int largest) {
+        int subArray =1;
+        int sum =0;
+
+        for(int i=0;i<nums.length;i++) {
+
+            if(sum+nums[i]<=largest) {
+                sum+=nums[i];
+            }else {
+                subArray++;
+                sum = nums[i];
+            }
+        }
+        return subArray;
+    }
+}
