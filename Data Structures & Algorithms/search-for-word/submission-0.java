4562a4c28074762ba35eb7e8713a2f36903f320f
shreekanti <shreekanti97@gmail.com>
2026-06-07 09:28:10 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+        Set<int[]> set = new HashSet<>();
+
+        for(int i=0;i<row;i++) {
+            for(int j=0;j<col;j++) {
+                if(dfs(i,j,0,word,board)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean dfs(int row, int col, int ind, String word, char[][] board) {
+        if(ind==word.length()) {
+            return true;
+        }
+
+        if(row<0 || row>=board.length || col<0 || col>= board[0].length ||
+        board[row][col]!=word.charAt(ind) || board[row][col] == '#')  {
+            return false;
+        }
+
+        board[row][col] = '#';
+
+        int[] dirr = {-1,0,1,0};
+        int[] dirc = {0,1,0,-1};
+        boolean res =false;
+
+        for (int i=0;i<4;i++) {
+            int nextr = row + dirr[i];
+            int nextc = col + dirc[i];
+             res = res || dfs(nextr,nextc,ind+1,word,board);
+           
+        }
+         board[row][col] = word.charAt(ind);
+        return res;
+         
+    }
+}
