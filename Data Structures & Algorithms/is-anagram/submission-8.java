5ccd62d5ce20d5f6d691df09cc000045588f3b68
shreekanti <shreekanti97@gmail.com>
2026-06-26 00:56:31 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //Brute
+        Map<Character, Integer> smap = new HashMap<>();
+        Map<Character, Integer> tmap = new HashMap<>();
+
+        for(int i=0;i<s.length();i++) {
+            smap.put(s.charAt(i), smap.getOrDefault(s.charAt(i),0)+1);
+        }
+
+        for(int i=0;i<t.length();i++) {
+            tmap.put(t.charAt(i), tmap.getOrDefault(t.charAt(i),0)+1);
+        }
+
+        return smap.equals(tmap);
+        // Tc -O(n) sc O(n)
+    }
+}
